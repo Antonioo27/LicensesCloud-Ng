5c1434a5008e0f8590288109403bca2eb8b1feba
Massimiliano Mattioli <test>
2024-11-21 16:22:40 +0100

    Componente prova

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ProvaComponent } from './prova/prova.component';
+import { LgcSharedModule } from './@shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProvaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LgcSharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
