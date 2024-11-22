57e18326d6138b42ca1d242fe8f037fa6fc6cca3
Massimiliano Mattioli <test>
2024-11-22 14:45:43 +0100

    Commit iniziale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProvaComponent } from './prova/prova.component';
 import { LgcSharedModule } from './@shared/shared.module';
+import { BlocksModule } from '@lgccommon/lib/blocks/blocks.module';
+import { ColorSelectorComponent } from './color-selector/color-selector.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProvaComponent
+    ProvaComponent,
+    ColorSelectorComponent
   ],
   imports: [
     BrowserModule,
-    LgcSharedModule
+    LgcSharedModule,
+    BlocksModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class ProvaComponent {
 
+  usernameList: { [key: string]: string } = {
+    "Antonio": "admin",
+    "Luca": "admin",
+    "Vincenzo": "admin"
+  };
+
+  Username: string = "Antonio";
+
+  Password: string = "admin";
+
+
+  onSaveData() {
+    if(this.Username in this.usernameList){
+      if(this.Password == this.usernameList[this.Username])
+        console.log("il bro c'è");
+
+    }
+    else
+      console.log("il bro non ce");
+    // controllo che esista lutente
+  }
+
 }
